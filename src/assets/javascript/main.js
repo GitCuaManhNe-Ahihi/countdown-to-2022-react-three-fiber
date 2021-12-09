9ea5c5068a8293c6d26accbc81f33988ae377109
Hoàng Văn Mạnh <61113572+GitCuaManhNe-hehe@users.noreply.github.com>
2021-12-09 19:01:31 +0700

    ok

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,4 +1,11 @@
-// var play2 = document.getElementById('play');
-// window.addEventListener('click', async function () {
-//    await play2.play();
-// });
+
+const widthh = window.innerWidth;
+if(widthh > 768){
+   var play2 = document.getElementById('play');
+   window.addEventListener('click', async function () {
+   await play2.play();
+});
+}
+window.addEventListener('resize', function () {
+   widthh = window.innerWidth;
+})
